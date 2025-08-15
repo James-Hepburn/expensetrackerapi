7a27af501d4b17686308fe4e16daad14259e0abf
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-15 13:36:13 -0600

    Repositories

--- src/main/java/com/example/expensetrackerapi/repository/ExpenseRepository.java
+++ src/main/java/com/example/expensetrackerapi/repository/ExpenseRepository.java
@@ -1,4 +1,14 @@
 package com.example.expensetrackerapi.repository;
 
-public class ExpenseRepository {
+import com.example.expensetrackerapi.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List <Expense> findByUser (User user);
+    List <Expense> findByUserAndDateBetween (User user, LocalDate start, LocalDate end);
 }
