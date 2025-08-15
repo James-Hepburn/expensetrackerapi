4b8cbe8050f640daf2c036e095853cd68fbaa84f
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-15 11:45:09 -0600

    User entity

--- src/main/java/com/example/expensetrackerapi/model/User.java
+++ src/main/java/com/example/expensetrackerapi/model/User.java
@@ -1,4 +1,29 @@
 package com.example.expensetrackerapi.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List <Expense> expenses;
+
+    public User (String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
