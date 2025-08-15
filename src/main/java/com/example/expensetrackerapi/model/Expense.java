446b61574652817b3c1e57bdb7c6ac6c717a5555
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-15 11:51:06 -0600

    Expense entity

--- src/main/java/com/example/expensetrackerapi/model/Expense.java
+++ src/main/java/com/example/expensetrackerapi/model/Expense.java
@@ -1,4 +1,38 @@
 package com.example.expensetrackerapi.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private double amount;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    public Expense (String description, double amount, LocalDate date, Category category) {
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+        this.category = category;
+    }
+
+    public enum Category {
+        GROCERIES, LEISURE, ELECTRONICS, UTILITIES, CLOTHING, HEALTH, OTHERS
+    }
 }
